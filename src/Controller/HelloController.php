55d12dc8995a57416a07dbe92d3bf4116ae8fcc9
Akash Vaidya <akash.vaidya23@gmail.com>
2025-08-25 08:49:19 +0530

    Auth --pending

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,11 +8,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
-    #[Route('/')]
-    public function index() :Response
-    {
-        return $this->render('hello/index.html.twig');
-        // $contents = $this->renderView('hello/index.html.twig');
-        // return new Response($contents);
-    }
+    // #[Route('/')]
+    // public function index() :Response
+    // {
+    //     return $this->render('hello/index.html.twig');
+    //     // $contents = $this->renderView('hello/index.html.twig');
+    //     // return new Response($contents);
+    // }
 }
